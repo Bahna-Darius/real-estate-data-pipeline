bc7f12a990d7a12abb3d129eafdfbe6b922debbd
Bahna-Darius <emanuelbahna@gmail.com>
2026-03-26 11:53:46 +0200

    fix: clean NBSP characters from price extraction

--- src/imobiliare_scraper.py
+++ src/imobiliare_scraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 import re
 
@@ -22,16 +23,17 @@ def test_scraper():
         soup = BeautifulSoup(response.text, 'html.parser')
 
         listings = soup.find_all("article")
-
         print(f"[*] Found {len(listings)} listings on this page!")
 
         if len(listings) > 0:
             print("\n--- Extracting ALL Prices ---")
 
+            scraped_data = []
+
             for index, listing in enumerate(listings, start=1):
                 # Extract Price
                 price_element = listing.find(string=re.compile("€"))
-                price = price_element.parent.get_text(strip=True) if price_element else "N/A"
+                price = price_element.parent.get_text(strip=True).replace('\xa0', '') if price_element else "N/A"
                 # Extract Area
                 area = "N/A"
                 m2_elements = listing.find_all(string=re.compile("m²"))
@@ -42,10 +44,9 @@ def test_scraper():
                         break
 
 
-                # Extract the link
+                # Extract the Title and Link
                 title = "N/A"
                 ad_url = "N/A"
-
                 all_links = listing.find_all("a", href=True)
 
                 for link in all_links:
@@ -60,10 +61,24 @@ def test_scraper():
                             ad_url = "https://www.storia.ro" + ad_url
                         break
 
+                listing_dict = {
+                    "Title": title,
+                    "Price": price,
+                    "Area": area,
+                    "Link": ad_url
+                }
+                scraped_data.append(listing_dict)
 
-                print(f"[{index}] {title}")
-                print(f" 💶 Price: {price} | 📐 Area: {area}")
-                print(f" 🔗 Link: {ad_url}\n")
+            # Transform list data in DF:
+            df = pd.DataFrame(scraped_data)
+            csv_filename = "storia_raw_data.csv"
+            json_filename = "storia_raw_data.json"
+
+            df.to_csv(csv_filename, index=False, encoding='utf-8-sig')
+            df.to_json(json_filename, orient="records", force_ascii=False, indent=4)
+
+            print(f"\n[+] Successfully saved {len(df)} records to {csv_filename} and {json_filename}!")
+
 
     else:
         print("[-] Still 0 listings. We might need to inspect the page structure further.")
